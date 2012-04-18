0052227e248112709610a558bd211e221fd35a29
Mark Paine <mark@mailbiter.com>
2012-04-17 19:53:47 -0700

    Remove the instance variables from the XPPAuthorization module methods

--- lib/paypal_permissions/x_pp_authorization.rb
+++ lib/paypal_permissions/x_pp_authorization.rb
@@ -5,23 +5,23 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module XPPAuthorization
       public
-      def x_pp_authorization_header url, access_token, access_token_verifier
+      def x_pp_authorization_header url, api_user_id, api_password, access_token, access_token_verifier
         timestamp = Time.now.to_i.to_s
-        signature = x_pp_authorization_signature url, timestamp, access_token, access_token_verifier
+        signature = x_pp_authorization_signature url, api_user_id, api_password, timestamp, access_token, access_token_verifier
         { 'X-PP-AUTHORIZATION' => "token=#{access_token},signature=#{signature},timestamp=#{timestamp}" }
       end
 
       public
-      def x_pp_authorization_signature url, timestamp, access_token, access_token_verifier
+      def x_pp_authorization_signature url, api_user_id, api_password, timestamp, access_token, access_token_verifier
         # no query params, but if there were, this is where they'd go
         query_params = {}
         key = [
-          URI.encode(@password),
+          URI.encode(api_password),
           URI.encode(access_token_verifier),
         ].join("&")
 
         params = query_params.dup.merge({
-          "oauth_consumer_key" => @login,
+          "oauth_consumer_key" => api_user_id,
           "oauth_version" => "1.0",
           "oauth_signature_method" => "HMAC-SHA1",
           "oauth_token" => access_token,
